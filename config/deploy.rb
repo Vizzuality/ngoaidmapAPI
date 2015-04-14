2b73ace2245d071f7ff7effc10241d6e986648e0
Miguel Mendoza <info@miguelmendoza.dj>
2015-04-14 21:36:46 +0200

    deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
+# config valid only for Capistrano 3.1
+lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ngo-api'
+set :repo_url, 'git@github.com:Vizzuality/ngoaidmapAPI.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, 'banners'
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/ubuntu/ngo-api'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,26 +23,50 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+#set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+#set :default_env, { path: "/usr/local/rvm/gems/ruby-2.1.2@global/bin/bundler" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+# set the locations that we will look for changed assets to determine whether to precompile
+set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc 'reload the database with seed data'
+  after :publishing, :seed do
+    on roles(:app), in: :sequence, wait: 5 do
+      within release_path do
+        if fetch(:rails_env) == "production"
+          execute :rake, 'db:migrate RAILS_ENV=production'
+          execute :rake, 'db:seed RAILS_ENV=production'
+        else
+          execute :rake, 'db:migrate RAILS_ENV=staging'
+          execute :rake, 'db:seed RAILS_ENV=staging'
+        end
+      end
     end
   end
+
+  desc 'Restart application'
+  after :seed, :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     within release_path do
+  #       execute :rake, 'memcached:flush RAILS_ENV=production'
+  #     end
+  #   end
+  # end
+
+  after :failed, :rollback
 
 end
