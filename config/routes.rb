ff11ff9e6e080b4c280301eb58a2f6b4c16c500e
CV-Gate <info@miguelmendoza.dj>
2015-04-24 16:30:51 +0200

    projects endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, path: nil, defaults: {format: 'json'} do
     #scope module: :v1, constraints: APIVersion.new(version: 1) do
     scope module: :v1 do
-      resources :projects
+      resources :projects, only: [:index, :show]
       resources :donors, only: [:show]
     end
   end
