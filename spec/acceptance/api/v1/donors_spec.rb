f88514733f2604cce5b288b46b7a8496c981b18a
Simao Belchior <simaobelchior@gmail.com>
2015-04-24 18:29:34 +0100

    Basic request working. though specs still failling

--- spec/acceptance/api/v1/donors_spec.rb
+++ spec/acceptance/api/v1/donors_spec.rb
@@ -1,12 +1,12 @@
 require 'acceptance_helper'
 
-resource 'GET /donors/:id' do
+resource 'Donors' do
   header "Accept", "application/json; application/vnd.esios-api-v1+json"
   header "Content-Type", "application/json"
   header 'Host', 'http://'
 
   let!(:donor) do
-    build(:donor)
+    create(:donor)
   end
 
   get "/donors/:id" do
@@ -18,7 +18,7 @@ resource 'GET /donors/:id' do
       expect(status).to eq(200)
       results = JSON.parse(response_body)['data']
       expect(results.length).to be == 1
-      expect(results[0]['name']).to  be == donor.name
+      expect(results[0]['name']).to  be == name
       expect(results[5]['url']).to   be == "/compare-groups/3"
     end
   end
