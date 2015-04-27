63e7f0622edf1c47bd733d8135d98690ce181745
Simao Belchior <simaobelchior@gmail.com>
2015-04-27 18:53:13 +0100

    Updated curl url on acceptance_helper. Regenerated docs

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -4,6 +4,6 @@ require 'support/factory_girl'
 
 RspecApiDocumentation.configure do |config|
   config.format    = [:json, :combined_text, :html]
-  config.curl_host = 'http://api.ngoaidmap.com'
+  config.curl_host = 'http://api.ngoaidmap.org'
   config.api_name  = "API NGO Aid Map"
 end
