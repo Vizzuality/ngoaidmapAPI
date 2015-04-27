ce31cf81d5758f249f45ac5b111080b5238278b4
Simao Belchior <simaobelchior@gmail.com>
2015-04-27 17:38:18 +0100

    Handling record_not_found errors with json api correct response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  private
+
+  def record_not_found
+    render json: {errors: [{ status: '404', title: 'Record not found' }] } ,  status: 404
+  end
 end
