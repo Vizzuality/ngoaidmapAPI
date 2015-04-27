0eefcced8d0f1f663611b9618f524e41d69cada4
Simao Belchior <simaobelchior@gmail.com>
2015-04-27 17:58:20 +0100

    Added spec to test for non existing records

--- spec/acceptance/api/v1/donors_spec.rb
+++ spec/acceptance/api/v1/donors_spec.rb
@@ -2,7 +2,7 @@ require 'acceptance_helper'
 
 resource 'Donors' do
   header "Accept", "application/json; application/vnd.api+json"
-  header "Content-Type", "application/json"
+  header "Content-Type", "application/vnd.api+json"
   header 'Host', 'http://api.ngoaidmap.org'
 
   let!(:donor) do
@@ -19,6 +19,15 @@ resource 'Donors' do
       results = JSON.parse(response_body)
       expect(results.length).to be == 1
       expect(results['data']['name']).to  be == name
+    end
+  end
+
+  get "/donors/:id" do
+    parameter :id, "A donor's id"
+    let(:id) { donor.id + 1 }
+
+    example_request "Get the show page for a non existing donor" do
+      expect(status).to eq(404)
     end
   end
 end
