5d687e30891ce8c4922c6c05aaa0ea5001709247
Simao Belchior <simaobelchior@gmail.com>
2015-04-28 09:53:15 +0100

    Using size instead of length: http://stackoverflow.com/questions/12253277/size-length-and-count-in-rails-3-2-3

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,7 +3,7 @@ module Api
     class ProjectsController < ApplicationController
       def index
         @projects = Project.fetch_all(project_params)
-        render json: @projects, root: 'data', meta: {total: @projects.length}, include: ['organization', 'sectors', 'donors', 'countries', 'regions']
+        render json: @projects, root: 'data', meta: {total: @projects.size}, include: ['organization', 'sectors', 'donors', 'countries', 'regions']
       end
 
       def show
